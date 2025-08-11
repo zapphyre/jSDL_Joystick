d523583111b3c85c684fa98926b5e8c5315d8053
zapphyre <mmanco89@gmail.com>
2025-08-11 19:16:51 +0200

    just add equalshashcode on sourcestate;

--- src/main/java/org/asmus/model/SourceState.java
+++ src/main/java/org/asmus/model/SourceState.java
@@ -1,12 +1,14 @@
 package org.asmus.model;
 
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Value;
 import lombok.extern.jackson.Jacksonized;
 
 @Value
 @Builder
 @Jacksonized
+@EqualsAndHashCode
 public class SourceState {
     boolean connected;
     String devicePath;
